16eb596d2b593bec836ac7e5295511fee348ed4c
Restop-2345 <michaelodumosu57@gmail.com>
2021-03-30 23:43:08 -0400

    UPDATE

--- API/bigquery/ML/vids/Python3/Working_With_Models/template.py
+++ API/bigquery/ML/vids/Python3/Working_With_Models/template.py
@@ -69,27 +69,161 @@ class my_bigquery_client():
                 
 
         # list models
+        if( env == "list_models"):
+            try:
+                models = client.list_models(dataset_main[0])  
+                schema = [
+                    "project",
+                    "dataset_id",
+                    "model_id"
+                ]
+                # for model in models:
+                #     print(model.reference.project)
+                result = {
+                    "schema":[{"field":x} for x in schema],
+                    "data":[
+                        # Row values can be accessed by field name or index.
+                        {
+                            schema[0]:row.reference.project,
+                            schema[1]:row.reference.dataset_id,
+                            schema[2]:row.reference.model_id  
+                        }
+                        for row in models
+                    ]
+                }
+                if(len(result["data"]) == 0):
+                    result["data"] = [
+                        {
+                            schema[0]:"No",
+                            schema[1]:"Models",
+                            schema[2]:"Here"
+                        }
+                        for row in [None]
+                    ]
 
+                return json.dumps(result)                 
+                
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print(e)
+                return 'an error occured check the output from the backend'   
         #
 
         # get model metadata
-        
+        elif(env == "get_model_metadata"):
+            try:
+                model_id = "{}.{}".format(dataset_main[0], name) 
+                model = client.get_model(model_id)
+                
+
+                return """\nModel id is {} 
+                Model friendly name is {}
+                Model created on {}
+                Model location {}
+                Model description {}""".format(model.model_id,model.friendly_name,model.created,model.location,model.description)
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print(e)
+                return 'an error occured check the output from the backend'           
         #
 
         # update model metadata
-        
+        elif(env == "update_model_metadata"):
+            try:
+                model_id = "{}.{}".format(dataset_main[0], name) 
+                model = client.get_model(model_id)  # Make an API request.
+                model.description = query
+                model = client.update_model(model, ["description"])  # Make an API request.
+
+                full_model_id = "{}.{}.{}".format(model.project, model.dataset_id, model.model_id)
+                return  "Updated model '{}' with description '{}'.".format(
+                        full_model_id, model.description
+                    )
+                                
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print('\n')
+                print(e)
+                return 'an error occured check the output from the backend'             
         #
 
         # copy a model
-        
+        elif(env == "copy_model"):
+            try:
+                
+                job_config = bigquery.job.CopyJobConfig(
+                    create_disposition  ="CREATE_IF_NEEDED",
+                    write_disposition  ="WRITE_TRUNCATE",
+                )
+                job = bigquery.job.CopyJob(
+                    job_config=job_config,
+                    sources = [
+                        bigquery.table.TableReference(
+                            dataset_ref = client.get_dataset(dataset_main[0]),
+                            table_id = name                    
+                        )]
+                    ,
+                    destination = 
+                        bigquery.table.TableReference(
+                            dataset_ref = client.get_dataset(dataset_main[0]),
+                            table_id = dest_name                
+                        )
+                    ,
+                    client= client,
+                    job_id = "my_copy_{}".format(uuid.uuid4())
+                )
+                job.result()  # Wait for the job to complete.
+                return "A copy of the model created."             
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print('\n')
+                print(e)
+                return 'an error occured check the output from the backend'        
         #
 
         # extract a model
-        
+        elif(env == "extract_model"):
+            try:
+                model_id = "{}.{}".format(dataset_main[0], name) 
+                model = client.get_model(model_id)
+
+                print(storage_buckets)
+                job = bigquery.job.ExtractJob(
+                    client= client,
+                    job_id = "my_copy_{}".format(uuid.uuid4()),
+                    source = model,
+                    destination_uris = storage_buckets,
+                    job_config= bigquery.job.ExtractJobConfig(
+                        destination_format="ML_TF_SAVED_MODEL" # ML_TF_SAVED_MODEL or ML_XGBOOST_BOOSTER                        
+                    )
+                )
+                job.result()
+                return "model {} export has been completed".format(name)
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print('\n')
+                print(e)
+                return 'an error occured check the output from the backend'            
         #
 
         # delete a model
-        
+        elif(env == "delete_model"):
+            try:
+                for name in models:
+                    model_id = "{}.{}".format(dataset_main[0], name)
+                    client.delete_model(model_id)  # Make an API request.
+                return "Deleted all models as requested"          
+            except BaseException as e:
+                print('my custom error\n')
+                print(e.__class__.__name__)
+                print('\n')
+                print(e)
+                return 'an error occured check the output from the backend'               
         #
         return "Check the backend env dictionary you did set it so the backend didnt do anything"
 
